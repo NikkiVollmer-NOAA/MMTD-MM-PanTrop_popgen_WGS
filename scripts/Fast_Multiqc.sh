a303c909ae9a4f35e2109a010f3c9d0cb8de113d
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-02-04 15:11:28 -0600

    Update and rename Fast_Multiqc.txt to Fast_Multiqc.sh

--- scripts/Fast_Multiqc.sh
+++ scripts/Fast_Multiqc.sh
@@ -9,6 +9,8 @@
 #SBATCH --partition=standard
 #SBATCH --time=6:00:00
 
+#might need to increase the time above if doing big files/directories
+
 source ~/.bashrc
 
 # load fastqc and multiqc
@@ -19,9 +21,12 @@ module load bio/fastqc/0.11.9
 
 cd /scratch2/nvollmer
 
+#can list all the directories for which there are reads to evaluate, separated by a space
 for input_dir in 250103_NOA016_RERUN_PanTrop_WGS 250115_NOA016_PanTrop_WGS_8Satt149-152
 do
     mkdir -p analysis/fastqc/${input_dir}/
+
+#double check if fastq files are named .fastq.gz or .fq.gz or something else and change the last part of line 28 accordingly
     fastqc -t 14 --noextract -o analysis/fastqc/${input_dir}/ ${input_dir}/*.fastq.gz
 
     multiqc analysis/fastqc/${input_dir}/ -o  analysis/fastqc/ --filename multiqc_report_${input_dir}
