365d1a6f4baa41054d5fd4897d5b69eed78c3ac9
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-02-17 13:38:30 -0600

    Update Align.sh

--- scripts/Align.sh
+++ scripts/Align.sh
@@ -12,8 +12,10 @@
 #SBATCH --array=[1-285]%25
 
 #More array info for line 12: The total number should be equal to the number of samples you want to run. 
-#It will run up to that number of jobs you put here: --array=[1-285]%25, so 285. 
+#It will run up to that number of jobs you put here: --array=[1-285]%25, so 285, and run 25 at a time.
 #If you have 600, then only the first 285 will run. If you have 100, but say 285, the 100 will run. So you should set it to the number of samples you have.
+#But if we are keeping all lanes per sample separate (see below) then need to could each Lane per sample as a "sample" - e.g. Satt001_L002 = sample #1, 
+#Satt001_L001 = sample #2, etc.
 
 #One of the largest directories timed out using a 2hr limit so went big with 24 hr. Per Giles "requesting 24 hrs for jobs that take 1 hr shouldn't be a problem. 
 #Its just the max amount of time that the job could possibly run, you're not really wasting resources if it only goes for one hour if that is what you're thinking"
@@ -30,8 +32,8 @@ outdir=/scratch2/nvollmer/analysis/alignment/241129_NOA016_PanTrop_WGS_Satt005_8
 fq1=$(find $indir -name "*_paired.R1.fq.gz" | sed -n $(echo $SLURM_ARRAY_TASK_ID)p)
 fq2=$(echo $fq1 | sed 's/_paired.R1.fq.gz/_paired.R2.fq.gz/g')
 sample=$(echo $fq1 | cut -f 6 -d "/" | cut -f 1 -d "_")
-#Per Reid decided to use separate library names for each sample. just to be super careful. why? "because you'll mark duplicates for each merged bam separately, I don't think it actually matters. 
-#but just in case, you can call each sample its own library." so made library variable be same as sample variable for the RG
+#Decided to keep separate all Lanes per Sample. Eg all Satt001 run on Lane 001 would be aligned into one bam - Satt001_L001.bam; and all Satt001 on Lane 002 would
+#be a separate bam - Satt001_L002.bam. So the sample variable is calling out the sample ID and the sample lane variable the Lane number.
 #OLD:library=$(echo $fq1 | cut -f 5 -d "/" | cut -f 2 -d "_")
 samplelane=$(echo $fq1 | cut -f 6 -d "/" | cut -f 3 -d "_")
 #library=$(echo $fq1 | cut -f 6 -d "/" | cut -f 1 -d "_")
