b4e3e2844ceef87d7cb661c44b5964875ac71d11
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-03-20 12:34:37 -0500

    Update picard.sh

--- scripts/picard.sh
+++ scripts/picard.sh
@@ -2,14 +2,14 @@
 #SBATCH -D /scratch2/nvollmer/log/array_jobs_picard
 #SBATCH --mail-type=END
 #SBATCH --mail-user=nicole.vollmer@noaa.gov
-#SBATCH --partition=medmem
+#SBATCH --partition=himem
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=100G
+#SBATCH --mem=96G
 #SBATCH --time=40:00:00
 #SBATCH --job-name=picard
 #SBATCH --output=%x.%A.%a.out
 #SBATCH --error=%x.%A.%a.err
-#SBATCH --array=[1-190]%8 
+#SBATCH --array=[1-190]%16 
 
 # can test with array of 1-2, but should be 1-190 for real run
 
@@ -27,6 +27,6 @@ inbam=$(ls *.bam | cut -f 1 -d "." | sed -n $(echo $SLURM_ARRAY_TASK_ID)p)
 
 echo $inbam
 
-#first am limiting memory usage in java to 96 - a little buffer so it doesn't reach 100 that is set in sbatch above
+#first am limiting memory usage in java to 96
 java -Xmx96g -jar $PICARD MarkDuplicates I=${indir}/${inbam}.combined.bam O=${outdir}/${inbam}_mkdup.bam TMP_DIR=/scratch2/nvollmer/analysis/Marked_Dups/tmp M=${outdir}/${inbam}.metrics.txt
 
