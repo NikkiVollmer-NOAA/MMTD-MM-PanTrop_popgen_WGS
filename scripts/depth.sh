3f98956fa2fa541d548b64c02c4b0a0f7a4467c3
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-04-14 09:27:48 -0500

    Create depth.sh

---
+++ scripts/depth.sh
@@ -0,0 +1,32 @@
+  GNU nano 2.9.8                                                               depth.sh
+
+#!/bin/bash
+#SBATCH -D /scratch2/nvollmer/log
+#SBATCH --mail-type=END
+#SBATCH --mail-user=nicole.vollmer@noaa.gov
+#SBATCH --partition=standard
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=12G
+#SBATCH --time=12:00:00
+#SBATCH --job-name=samtools
+#SBATCH --output=%x.%A.%a.out
+#SBATCH --error=%x.%A.%a.err
+
+
+module load bio/samtools/1.19
+
+cd /scratch2/nvollmer/analysis/Clipped/
+
+for sample in *.bam
+
+do
+
+f1=${sample%.bam}
+#need to create a folder in Clipped called Depth
+samtools depth $sample>Depth/$f1-stats.txt
+
+done
+
+## then once that is done running, can go into Depth folder and used following to pull out first row in each file (which has info on depth) 
+##and then puts it all into a text file 
+head -1 *stats.txt > test.txt
