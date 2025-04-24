a16e61dc60aac371b15ab37963d2e088ae67b1c8
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-04-24 13:29:51 -0500

    Update realign_index.sh

--- scripts/realign_index.sh
+++ scripts/realign_index.sh
@@ -14,9 +14,6 @@ module load bio/samtools
 
 bams=/scratch2/nvollmer/analysis/Clipped/
 
-#go to your marked bams, take every file labeled .bam (samplename_mkdup.bam) and first remove everything after the '.' = bam
-#next remove everything after the '_' = mkdup so all you are left with is a list of sample names
-#then last bit is all for running the array
 cd $bams
 inbam=$(ls *.bam | sed -n $(echo $SLURM_ARRAY_TASK_ID)p)
 
