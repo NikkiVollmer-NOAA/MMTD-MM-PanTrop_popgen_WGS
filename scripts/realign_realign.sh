d3fc3471e69249a1f860f0016f4d2fb8a9d20050
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-05-28 16:45:50 -0500

    Update realign_realign.sh

--- scripts/realign_realign.sh
+++ scripts/realign_realign.sh
@@ -2,9 +2,9 @@
 #SBATCH -D /scratch2/nvollmer/log
 #SBATCH --mail-type=END
 #SBATCH --mail-user=nicole.vollmer@noaa.gov
-#SBATCH --partition=himem
+#SBATCH --partition=medmem
 #SBATCH --mem=120G
-#SBATCH --time=99:00:00
+#SBATCH --time=10-00
 #SBATCH --job-name=realign
 #SBATCH --output=%x.%A.%a.out
 #SBATCH --error=%x.%A.%a.err
@@ -21,12 +21,12 @@ BAMLIST=/scratch2/nvollmer/analysis/Clipped/bam_list.list #make sure file name e
 REFERENCE=/scratch2/nvollmer/refseq/Stenella_attenuata_HiC.fasta #need to make sure reference is uncompressed and there is a .fai and .dict file - see gatk_fasta.txt
 
 ## Create list of potential in-dels
-java -Xmx96g -jar $GATK \
-	   -T RealignerTargetCreator \
-	   -R $REFERENCE \
-	   -I $BAMLIST \
-	   -o $BASEDIR'/all_samples_for_indel_realigner.intervals' \
-	   -drf BadMate
+#java -Xmx96g -jar $GATK \
+#	   -T RealignerTargetCreator \
+#	   -R $REFERENCE \
+#	   -I $BAMLIST \
+#	   -o $BASEDIR'/all_samples_for_indel_realigner.intervals' \
+#	   -drf BadMate
 
 cd /scratch2/nvollmer/analysis/realign
 
@@ -35,6 +35,6 @@ java -Xmx96g -jar $GATK \
    -T IndelRealigner \
    -R $REFERENCE \
    -I $BAMLIST \
-   -targetIntervals $BASEDIR'/all_samples_for_indel_realigner.intervals' \
+   -targetIntervals $BASEDIR'/intervals/concat.intervals' \
    --consensusDeterminationModel USE_READS  \
    --nWayOut _realigned.bam
