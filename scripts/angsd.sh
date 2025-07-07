b691f785a85d3eccacd439b65e0bbeac52f42e2d
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-07-07 16:40:06 -0500

    Update angsd.sh

--- scripts/angsd.sh
+++ scripts/angsd.sh
@@ -9,21 +9,19 @@
 #SBATCH --job-name=angsd
 #SBATCH --output=%x.%A.%a.out
 #SBATCH --error=%x.%A.%a.err
+#SBATCH --array=1-21%4 #just running the first 21 scaffolds 
 
 module load bio/angsd/0.940
 module load bio/samtools/1.19
 
 BASEDIR=/scratch2/nvollmer/analysis/Clipped
 REFERENCE=/scratch2/nvollmer/refseq/Stenella_attenuata_HiC.fasta
+SCAFFOLD_DIR=/scratch2/nvollmer/refseq
 
-#looking at one chromosome at the first 500k bp for a faster test of the code
-#these filters will retain only uniquely mapping reads, not tagged as bad, considering only proper pairs, without trimming, 
-#and adjusting for indel/mapping (as in samtools). -C 50 reduces the effect of reads with excessive mismatches, 
-#while -baq 1 computes base alignment quality as explained here (BAQ) used to rule out false SNPs close to INDELS.
-angsd -b  $BASEDIR/ANGSD_bams.txt -ref $REFERENCE -out $BASEDIR/ANGSD \
+angsd -b  $BASEDIR/ANGSD_bams.txt -ref $REFERENCE -out $BASEDIR/ANGSD -rf $SCAFFOLD_DIR/ref_chrom.txt \
 	-uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -trim 0 -minMapQ 25 -minQ 25 -skipTriallelic 1 \
-  	-setMinDepth 3 -minInd 142 -SNP_pval 1e-6 -doMajorMinor 1 -doMaf 1 -minMAF 0.05 \
- 	 -GL 1 -doGLF 2 \ #2=GATK or 1=Samtools; 4=output in text format or 2=beagle and seems popular
+  	-doCounts 1 -setMinDepth 3 -minInd 142 -SNP_pval 1e-6 -doMajorMinor 1 -doMaf 1 -minMAF 0.05 \
+ 	-GL 1 -doGLF 2 \ #2=GATK or 1=Samtools; 4=output in text format or 2=beagle and seems popular
 
 #	-r HiC_scaffold_1:1-500000
 #####unsure if I should use anything below this line for first run, those with ## are in the code above, those with # are not
@@ -34,7 +32,7 @@ angsd -b  $BASEDIR/ANGSD_bams.txt -ref $REFERENCE -out $BASEDIR/ANGSD \
 #-setMaxDepth 30 #Filters out sites where the total depth across all individuals exceeds a threshold. Requires -doCounts
 #-setMinDepthInd #Discard individual if sequencing depth for an individual is below [int]. This filter is only applied to analysis which are based on counts of alleles i.e. analysis that uses -doCounts
 #-setMaxDepthInd #Discard individual if sequencing depth for an individual is above [int]. This filter is only applied to analysis which are based on counts of alleles i.e. analysis that uses -doCounts
-#-doCounts 1 ##count # ATCG at all sites and samples
+##-doCounts 1 ##count # ATCG at all sites and samples
 ##-SNP_pval 1e-6 #Remove sites with a pvalue larger
 #-sb_pval 1e-6
 #-hwe_pval 0.05
