62b24e22e1aa537c247f061863b90e0f67090757
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-07-08 16:21:14 -0500

    Update angsd.sh

--- scripts/angsd.sh
+++ scripts/angsd.sh
@@ -3,8 +3,8 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=nicole.vollmer@noaa.gov
 #SBATCH --partition=himem
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=200G
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=20G
 #SBATCH --time=24:00:00
 #SBATCH --job-name=angsd
 #SBATCH --output=%x.%A.%a.out
@@ -21,7 +21,7 @@ SCAFFOLD_DIR=/scratch2/nvollmer/refseq
 angsd -b  $BASEDIR/ANGSD_bams.txt -ref $REFERENCE -out $BASEDIR/ANGSD -rf $SCAFFOLD_DIR/ref_chrom.txt \
 	-uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -trim 0 -minMapQ 25 -minQ 25 -skipTriallelic 1 \
   	-doCounts 1 -setMinDepth 3 -minInd 142 -SNP_pval 1e-6 -doMajorMinor 1 -doMaf 1 -minMAF 0.05 \
- 	-GL 1 -doGLF 2 \ #2=GATK or 1=Samtools; 4=output in text format or 2=beagle and seems popular
+ 	-GL 1 -doGLF 2 -nThreads 6 -doVcf 1\ #2=GATK or 1=Samtools; 4=output in text format or 2=beagle and seems popular
 
 #	-r HiC_scaffold_1:1-500000
 #####unsure if I should use anything below this line for first run, those with ## are in the code above, those with # are not
@@ -41,7 +41,7 @@ angsd -b  $BASEDIR/ANGSD_bams.txt -ref $REFERENCE -out $BASEDIR/ANGSD -rf $SCAFF
 #-mapQ_pval 1e-4
 ##-minMAF #remove sites with MAF below
 ##-doMaf	 #Calculate persite frequencies '.mafs.gz'
-#-P or -nThreads #sets # of threads to use
+##-P or -nThreads #sets # of threads to use
 ##-doMajorMinor 1 #Infer the major/minor using different approaches - 1= from GL; 4 = use ref allele as major, requires ref
 #-geno_minDepth #Only call genotypes if the depth is as least [int] for that individuals This requires -doCounts and -doGeno
 
