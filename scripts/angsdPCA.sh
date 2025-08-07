e829cf5b9b733940695b601817fc90a0f3aa32e5
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-08-07 10:46:12 -0500

    Update angsdPCA.sh

--- scripts/angsdPCA.sh
+++ scripts/angsdPCA.sh
@@ -21,4 +21,7 @@ angsd sites index $SNPLIST
 
 
 angsd -b $BASEDIR/ANGSD_bams.txt -anc $REF -out $BASEDIR/ANGSDresults/GLF_2/ngsLD_25kb/PCA_LDpruned \
-    -GL 1 -doGlf 2 -doMajorMinor 3 -doMAF 1 -doPost 1 -doIBS 1 -doCounts 1 -doCov 1 -makeMatrix 1 -sites $SNPLIST
+    -GL 1 -doGlf 2 -doMajorMinor 3 -doMAF 1 -doPost 1 -doIBS 1 -doCounts 1 -doCov 1 -makeMatrix 1 \
+    -rf $BASEDIR/ANGSDresults/GLF_2/ngsLD_25kb/chrs.txt -sites $SNPLIST
+
+#chrs.txt is a text file that list the name of the 1st 21 scaffolds so that the analysis is limited to just those
