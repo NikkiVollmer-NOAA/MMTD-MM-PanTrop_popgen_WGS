4057ba5b58f5d977c120497af3f5a7604c72291c
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2025-10-30 10:26:17 -0500

    Update ngsadmix.sh

--- scripts/ngsadmix.sh
+++ scripts/ngsadmix.sh
@@ -9,7 +9,7 @@
 #SBATCH --job-name=ngsadmix
 #SBATCH --output=%x.%A.%a.out
 #SBATCH --error=%x.%A.%a.err
-#SBATCH --array=1-50 #<-- 5 K-values (2-6) * 10 replicates = 50 jobs
+#SBATCH --array=[1-100]10% #<-- 10 K-values (2-6) * 10 replicates = 50 jobs; run 10 at a time
 
 module load bio/ngsadmix/32
 
@@ -17,8 +17,8 @@ BASEDIR=/scratch2/nvollmer/analysis/Clipped/Clipped_Realigned
 
 # We have 10 replicates for each K
 REPS_PER_K=10
-# Our K-values start at 2
-K_OFFSET=2
+# Our K-values start at 1
+K_OFFSET=1
 
 # Calculate K_VALUE and REP_NUM from the SLURM_ARRAY_TASK_ID (1-50)
 # We use ($SLURM_ARRAY_TASK_ID - 1) for 0-based indexing to make math easier
