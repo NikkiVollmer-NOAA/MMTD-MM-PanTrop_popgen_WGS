d669b7da0ef990a1ce59b7517c5990e1a261d3d6
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2026-03-23 19:18:13 -0500

    Update bcf_plink.sh

--- scripts/bcf_plink.sh
+++ scripts/bcf_plink.sh
@@ -13,35 +13,41 @@
 
 
 module load bio/bcftools
-module load bio/plink/2.00a5.10
+module load bio/plink/1.90b6.23
 
 cd /scratch2/nvollmer/analysis/Clipped/Clipped_Realigned/ANGSDresults/vcf/
 
 # Use --threads to speed up the process
-# --vcf-half-call h handles any missing/ambiguous genotypes safely
-bcftools view -v snps -m2 -M2 -Ou merged_data_noX.bcf.gz | \
-plink2 --vcf /dev/stdin \
+# --vcf-half-call handles any missing/ambiguous genotypes safely
+bcftools view -v snps -m2 -M2 -Ov merged_data_noX.bcf.gz | \
+plink --vcf /dev/stdin \
        --make-bed \
        --out my_data_final \
+       --memory 60000 \
        --threads 8 \
-       --vcf-half-call h \
+       --vcf-half-call missing \
+       --double-id \
        --allow-extra-chr
 
 # 2. Identify SNPs for Pruning
 # Fixed: Changed 'full_data' to 'my_data_final' to match the step above
-# pruning using sliding window, windows are 50kb in this code, and am calculating the correlation (r2) 
+# pruning using sliding window, windows are 50kb in this code, and am calculating the correlation (r2)
 # between all pairs of SNPs in a window, if any pair is highly correlated (r2>0.1) one SNP is removed
 # It then shifts 5 SNPs forward and repeats the process
-plink2 --bfile my_data_final \
+plink  --bfile my_data_final \
        --indep-pairwise 50 5 0.1 \
        --out prune_it \
        --threads 8 \
+       --memory 60000 \
+       --double-id \
        --allow-extra-chr
 
 # 3. Create the pruned dataset
-plink2 --bfile my_data_final \
+plink --bfile my_data_final \
        --extract prune_it.prune.in \
        --make-bed \
        --out admixtools_ready \
        --threads 8 \
+       --memory 60000 \
+       --double-id \
        --allow-extra-chr
