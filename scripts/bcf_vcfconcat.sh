8f2d3196886b2b47db22c3a18f335b2dcb8a2135
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2026-06-03 13:54:58 -0500

    Create bcf_vcfconcat.sh

---
+++ scripts/bcf_vcfconcat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -D /scratch2/nvollmer/log
+#SBATCH --mail-type=END
+#SBATCH --mail-user=nicole.vollmer@noaa.gov
+#SBATCH --partition=standard
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=12:00:00
+#SBATCH --job-name=bcfconcat
+#SBATCH --output=%x.%A.%a.out
+#SBATCH --error=%x.%A.%a.err
+
+
+module load bio/bcftools
+
+cd /scratch2/nvollmer/analysis/Clipped/Clipped_Realigned/ANGSDresults/vcf/
+
+# 1. Create a list of all your final filtered scaffold VCFs
+ls final_8_HiC_scaffold_*.vcf.gz > vcf_list.txt
+
+# 2. Concatenate (stitch) them back together in order
+bcftools concat -f vcf_list.txt -Oz -o final_8_filtered_master.vcf.gz
+
+# 3. Index the final master file
+tabix -p vcf final_8_filtered_master.vcf.gz
